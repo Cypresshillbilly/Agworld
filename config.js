0f208c1e19b5c9ccb2ed47425c93c29802036913
Cypresshillbilly <nico.cuaasa@gmail.com>
2026-09-06 16:59:06 +0200

    Use stable national DLRRD municipality and town GIS layers

--- config.js
+++ config.js
@@ -6,8 +6,7 @@ window.AG_WORLD_CONFIG = {
 // independently; if either remote layer is unavailable the base Google map
 // still initializes normally.
 const AGWORLD_SPATIAL_SOURCES = {
-  municipalities: 'https://nspdr.dlrrd.gov.za/server/rest/services/NationalDatasets/MunicipalDemarcationBoard/MapServer/3/query?where=1%3D1&outFields=*&returnGeometry=true&outSR=4326&f=geojson',
-  // National Surveyor-General town polygons, rather than the previous
-  // Western Cape-hosted AfriGIS endpoint.
-  towns: 'https://law.drdlr.gov.za/server/rest/services/LAW_Spatial_Admin_Boundaries/MapServer/130/query?where=1%3D1&outFields=*&returnGeometry=true&outSR=4326&f=geojson'
+  // Stable national DLRRD Surveyor-General ArcGIS services in WGS84.
+  municipalities: 'https://law.drdlr.gov.za/server/rest/services/LAW_Spatial_Admin_Boundaries/MapServer/115/query?where=1%3D1&outFields=MUNICNAME%2CMUNICCODE%2COBJECTID&returnGeometry=true&outSR=4326&f=geojson',
+  towns: 'https://law.drdlr.gov.za/server/rest/services/LAW_Spatial_Admin_Boundaries/MapServer/130/query?where=1%3D1&outFields=S12_NAME%2CTOWN_EXT%2CSGTOWN%2COBJECTID&returnGeometry=true&outSR=4326&f=geojson'
 };
